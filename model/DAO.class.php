f605dbf86f4cca4b4e17f43151f1e05f926ad5e2
BERTONI Felix <bertonif@hostname.iut2.upmf-grenoble.fr>
2015-10-14 14:28:12 +0200

    suite DBB, PLM celle des profs

--- model/DAO.class.php
+++ model/DAO.class.php
@@ -1,4 +1,7 @@
 <? php
+include_once("RSS.class.php");
+include_once("Nouvelle.class.php");
+
 class DAO {
         private $db; // L'objet de la base de donnée
 
@@ -39,7 +42,15 @@ class DAO {
 
         // Acces à un objet RSS à partir de son URL
         function readRSSfromURL($url) {
-          ...
+         	//construction de la requete
+				$q = "SELECT * FROM RSS WHERE url='".$url."'";
+				$r = $this->db->query($q);
+				//envoi du resultat (== NULL si resultat vide)
+				if (!$r){
+					return NULL;
+				} else {
+				return $r;
+			}
         }
 
         // Met à jour un flux
@@ -63,18 +74,51 @@ class DAO {
 
         // Acces à une nouvelle à partir de son titre et l'ID du flux
         function readNouvellefromTitre($titre,$RSS_id) {
-          ...
+			  //construction de la requete
+ 			 $q = "SELECT * FROM nouvelle WHERE titre='".$titre."' and RSS_id='".$RSS_id."'";;
+ 			 $r = $this->db->query($q);
+ 			 //envoi du resultat (== NULL si resultat vide)
+ 			 if (!$r){
+ 				 return NULL;
+ 			 } else {
+ 			 return $r;
+ 		 	 }
         }
 
         // Crée une nouvelle dans la base à partir d'un objet nouvelle
         // et de l'id du flux auquelle elle appartient
         function createNouvelle(Nouvelle $n, $RSS_id) {
-          ...
+			  $news = $this->readNouvellefromTitre($n->titre, $RSS_id);
+			  if ($new == NULL) {
+				 try {
+					$q = "INSERT INTO nouvelle (titre, RSS_id) VALUES ('.$n->titre.', '.$RSS_id.')";
+					$r = $this->db->exec($q);
+					if ($r == 0) {
+					  die("createRSS error: no rss inserted\n");
+					}
+					return $this->readNouvellefromTitre($n->titre, $RSS_id);
+				 } catch (PDOException $e) {
+					die("PDO Error :".$e->getMessage());
+				 }
+			  } else {
+				 // Retourne l'objet existant
+				 return $news;
+			  }
         }
 
         // Met à jour le champ image de la nouvelle dans la base
         function updateImageNouvelle(Nouvelle $n) {
-          // Met à jour uniquement le titre et la date
-          ...
+			  // Met à jour uniquement le titre et la date
+           $titre = $this->db->quote($rss->titre());
+           $q = "UPDATE nouvelle SET "
+           try {
+             $r = $this->db->exec($q);
+             if ($r == 0) {
+               die("updateRSS error: no news updated\n");
+             }
+           } catch (PDOException $e) {
+             die("PDO Error :".$e->getMessage());
+           }
+
         }
 ?>
